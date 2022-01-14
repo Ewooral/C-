545cd00abc4524e872636991e8512f866c7468c9
Ewooral <boahen.cos@gmail.com>
2022-01-14 17:25:17 +0000

    constructor function in c++ update

--- constructor_function.cpp
+++ constructor_function.cpp
@@ -4,11 +4,16 @@ using namespace std;
 // A class is the specification, template or blueprint of a datatype
 class Car{
    public:
-      //creating a contructor function 
-      Car(string carName, string carType, string carRegistryNumber){
-          cout << "the name of my car is " << carName << endl;
-          cout << "the type of my car is " << carType << endl;
-          cout << "the car registry number is " << carRegistryNumber << endl;
+       string carName;
+       string carType;
+       string carRegistryNumber;
+       string dateManufactured;
+       // creating a contructor function
+       Car(string carname, string cartype, string carregistrynumber)
+       {
+           carName = carname;
+           carType = cartype;
+           carRegistryNumber = carregistrynumber;
       }
 
 };
@@ -18,6 +23,16 @@ int main()
     Car italianCars("Lamborghini", "F-Force Ario", "2022-02-28"); // 1st instaqnce of the class
 
     Car americanCars("Ferrari", "IO222-Heaven", "2013-20-02"); // 2nd instance of the same class
+
+    cout << "-------------1st instance of the class---------" << endl;
+    cout << "the name of my car is " << italianCars.carName << endl;
+    cout << "the type of my car is " << italianCars.carType << endl;
+    cout << "the car registry number is " << italianCars.carRegistryNumber << endl;
+
+    cout << "-------------2nd instance of the class---------" << endl;
+    cout << "the name of my car is " << americanCars.carName << endl;
+    cout << "the type of my car is " << americanCars.carType << endl;
+    cout << "the car registry number is " << americanCars.carRegistryNumber << endl;
 
     return 0;
 }
