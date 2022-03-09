7603ea1527ed7f225684f396fb5ceabb6d471d67
Ewooral <boahen.cos@gmail.com>
2022-03-09 15:47:10 +0000

    c and c++ fundamentals

--- variables.cpp
+++ variables.cpp
@@ -9,10 +9,13 @@ int main()
 
     //information lost. less safe than brace initializers
     int narrowing_conversion_function(34.09);
+    double decimal{34.221};
 
     cout << apple_count << "\n" << apple_countI << "\n" <<
-     narrowing_conversion_function<< endl;
+     narrowing_conversion_function << "\n" << decimal << endl;
 
+   // Check the size of your variables in memory with sizeof function
+   cout << "size of int: " << sizeof(int) << endl;
 
 string myFirstName = "Elijah";
 string myLastName = "Boahen";
