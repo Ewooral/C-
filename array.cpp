76eb1e9cb32514cc226d2835e807322e87447874
Ewooral <boahen.cos@gmail.com>
2022-03-10 09:21:11 +0000

    array in c language

--- array.cpp
+++ array.cpp
@@ -7,4 +7,15 @@ int main()
     cout << Nums[2] << endl; // print out the number 15 at position 2
     Nums[23] = 455;
     cout << Nums[23] << endl;
+
+    int A[5] = {0};
+    cout << A[3] << endl;
+
+
+    // ways of accessing elements in an array.
+    int B[] = {2, 4, 5, 2, 3};
+    cout << *(B +2) << endl; // 5
+    cout << *(B + 1) << endl; // 4
+    cout << B[3] << endl; // 2
+    cout << 4[B] << endl; // 3
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,12 +1,2 @@
-    // string isSenior()
-    // {
-    //     if (age <= 30)
-    //     {
-    //         cout << "You're a Junior" << endl;
-    //     }
-    //     else
-    //     {
-    //         cout << "You're a Senior" << endl;
-    //     }
-    //     return 0;
-    // }
+d;
+int main()
